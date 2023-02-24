d202525a3c6b6082ba71b0364e283c33c0eb4271
Mike Silvers <mikesilvers@gmail.com>
2023-02-24 19:33:14 -0400

    Removed unneeded code from the table view cell

--- msilversTakeHome/MainView/MSTTableViewCell.swift
+++ msilversTakeHome/MainView/MSTTableViewCell.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class MSTTableViewCell: UITableViewCell {
 
+    /// the top label of the table cell
     @IBOutlet weak var nameLabel: UILabel!
     
+    /// the center label of the table cell
     @IBOutlet weak var emailLabel: UILabel!
     
+    /// the bottom label of the table cell
     @IBOutlet weak var phoneLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
