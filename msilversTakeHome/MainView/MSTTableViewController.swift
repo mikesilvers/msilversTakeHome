1560dc898f717eb64d5882f36a8d070ad58fbfea
Mike Silvers <mikesilvers@gmail.com>
2023-02-24 19:23:36 -0400

    Removed some commented out template code

--- msilversTakeHome/MainView/MSTTableViewController.swift
+++ msilversTakeHome/MainView/MSTTableViewController.swift
@@ -55,16 +55,6 @@ class MSTTableViewController: UITableViewController {
         return cell
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     // MARK: - Supporting functions
     
     /// Process the cell and setup the cell labels
