88c69589f840bd0c71c232e851a7756b82640004
Mike Silvers <mikesilvers@gmail.com>
2023-02-25 13:58:47 -0400

    Added the network call and the no network error in using combine
    Signed-off-by: Mike Silvers <mikesilvers@gmail.com>

---
+++ msilversTakeHome/Models/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  msilversTakeHome
+//
+//  Created by Mike Silvers on 2/24/23.
+//
+
+import Foundation
---
+++ msilversTakeHome/Networking/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  msilversTakeHome
+//
+//  Created by Mike Silvers on 2/24/23.
+//
+
+import Foundation
